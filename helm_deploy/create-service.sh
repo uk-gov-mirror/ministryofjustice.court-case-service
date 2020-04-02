976f636667a84d11105d34b8d72915bc177af84e
Robin Sillem <robin.sillem@digital.justiice.gov.uk>
2020-04-02 08:49:16 +0100

    Demo of composable transient environments. Not intended for merging as is

--- helm_deploy/create-service.sh
+++ helm_deploy/create-service.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # Creates a transient court-case-service pod
 
-[ $# -ne 5 ] && { echo "Usage: $0 namespace env_name court_case_service_path values_file cert_secret"; exit 1; }
+[ $# -ne 6 ] && { echo "Usage: $0 namespace env_name court_case_service_path values_file domain cert_secret"; exit 1; }
 
 # Set vars
 namespace=$1                                                       # kubernetes namespace
 env_name=$2                                                        # environment name
 court_case_service_path=$3                                         # Location of court case service source
 values_file=$4                                                     # The helm values for this deployment
+domain=$5                                                          # host domain
+cert_secret=$6                                                     # TLS cert secret name
 deployment_name=court-case-service-$env_name                       # deployment name
 db_secret_name=court-case-service-$env_name-rds-instance-output    # k8s database secret name
 service_secret_name=court-case-service-$env_name-secrets           # k8s service secret name
-cert_secret=$5                                                     # TLS cert secret name
 
 echo "Creating a court-case-service deployment. Namespace: $namespace, deployment: $deployment_name..."
 
@@ -20,7 +21,7 @@ helm install $court_case_service_path/helm_deploy/court-case-service \
 --values $values_file \
 --set secrets.db.secret_name=$db_secret_name \
 --set secrets.service.secret_name=$service_secret_name \
---set ingress.hosts[0].host=$deployment_name \
+--set ingress.hosts[0].host=$deployment_name.$domain \
 --set ingress.hosts[0].cert_secret=$cert_secret \
 --wait \
 --namespace $namespace \
