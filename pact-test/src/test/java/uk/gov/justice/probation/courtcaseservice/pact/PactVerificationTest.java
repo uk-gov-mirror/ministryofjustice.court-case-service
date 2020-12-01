7c0bfb21f01b121e1e694847fae3544c649eb60c
John Evans <john.evans@digital.justice.gov.uk>
2020-12-01 14:22:38 +0000

    WIP

--- pact-test/src/test/java/uk/gov/justice/probation/courtcaseservice/pact/PactVerificationTest.java
+++ pact-test/src/test/java/uk/gov/justice/probation/courtcaseservice/pact/PactVerificationTest.java
@@ -1,32 +1,22 @@
 package uk.gov.justice.probation.courtcaseservice.pact;
 
-
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.Provider;
-import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@Provider("court-case-service")
-@ActiveProfiles("test")
+@Provider("offender-detail-provider")
 @PactBroker(scheme = "https",
         host = "moj-court-case-service.pactflow.io",
-        authentication = @PactBrokerAuth(token = "d2CE9t3IDOL0wJ8mR_QZrw"))
+        authentication = @PactBrokerAuth(token = "BePmnJNfr6Ka_6BT2InkBA")
+)
 public class PactVerificationTest {
-
   @TestTemplate
   @ExtendWith(PactVerificationInvocationContextProvider.class)
   void pactVerificationTestTemplate(PactVerificationContext context) {
     context.verifyInteraction();
-  }
-
-  @State("An offender exists")
-  public void noop() {
   }
 }
